4ec97793552424efeac3eb26288f128ba3b5735c
Richard Steen <richard.steen@activenetwork.com>
2016-07-18 21:58:17 -0500

    Extract API URL env var

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.APP.API_URL = 'http://localhost:4000';
     ENV.contentSecurityPolicy = {
       'default-src': "'none'",
       'script-src': "'self'",
@@ -44,7 +45,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.APP.API_URL = process.env.API_URL
   }
 
   return ENV;
